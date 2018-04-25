afa9b908fe493ea2d5804a83e64ffbc136030413
Abhishek Sah <sah.1@iitj.ac.in>
2018-04-25 14:34:41 +0530

    Adds Item-Item Collaborative Filter

--- item-item.py
+++ item-item.py
@@ -1,7 +1,7 @@
-
 import csv
 from decimal import Decimal
 import math
+import time
 def maxm(a, b):
     if a>=b:
         return a
@@ -35,13 +35,15 @@ def sim(item1, item2):
     prod2=math.sqrt(prod2)
 
     fprod=Decimal(prod1*prod2)
-
-    return Decimal(psum/fprod)
+    if fprod==0:
+        fprod=1
+    return (psum/fprod)
 
 fname="utility.txt"
 dataset=[]
 itemCount=0
 userCount=0
+start_time = time.time()
 
 with open(fname) as tsv:
     for line in csv.reader(tsv, dialect="excel-tab"):
@@ -68,14 +70,59 @@ for i in range(itemCount+1):
         array.append(0)
     utilityMatrix.append(array)
 print len(dataset)
-
 for i in range(len(dataset)-1):
     utilityMatrix[dataset[i][1]][dataset[i][0]]=dataset[i][2]
+#
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print utilityMatrix[i][j],
+#     print "\n"
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        print utilityMatrix[i][j],
-    print "\n"
+userx=input("Which user to give suggestion for ? ")
+itemx=input("Which items expected rating ? ")
 
-for i in range(1, itemCount):
-    print sim(utilityMatrix[1], utilityMatrix[i])
+
+item_index = []
+for i in range(1, itemCount+1):
+	item_index.append(i)
+# print item_index
+
+thefile = open('simArray.txt', 'w')
+
+simArray=[]
+for i in range(1, itemCount+1):
+    simArray.append(sim(utilityMatrix[itemx], utilityMatrix[i]))
+print len(simArray)
+# weightedSimArray=[]
+
+# for i in range(0, len(simArray)):
+#     if i!=itemx:
+#         weightedSimArray.append(simArray[i]*utilityMatrix[i][userx])
+#     else:
+#         weightedSimArray.append(simArray[i])
+
+for x in simArray:
+  thefile.write("%s\n" % x)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# print len(weightedSimArray)
+
+# print weightedSimArray[0]
+
+
+top_50 = [x for (y,x) in sorted(zip(simArray, item_index), key=lambda pair: pair[0], reverse=True)]
+top_50 = top_50[:50]
+
+# print top_50
+
+weightedSum=0
+denom=0
+for i in range(0, len(top_50)):
+    weightedSum=weightedSum+(utilityMatrix[top_50[i]][userx]*simArray[top_50[i]])
+    multp=1
+    if utilityMatrix[top_50[i]][userx]==0:
+        multp=0
+    denom=denom+(simArray[top_50[i]]*multp)
+
+print "predicted rating : ", Decimal(weightedSum/denom)
